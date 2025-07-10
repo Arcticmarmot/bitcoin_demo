dd2c5ff4c2ce2f618d4f50156a38bfd4b370bb28
marmot <arctic_marmot@outlook.com>
2025-07-10 15:16:58 +0800

    chore: 编写tx类

--- ecc/helper.py
+++ ecc/helper.py
@@ -34,4 +34,37 @@ def encode_base58(s):
 def encode_base58_checksum(b):
     return encode_base58(b + hash256(b)[:4])
 
+def little_endian_to_int(b):
+    """little_endian_to_int takes byte sequence as a little-endian number.
+    Returns an integer"""
+    return int.from_bytes(b, 'little')
+
+def int_to_little_endian(n, length):
+    """endian_to_little_endian takes an integer and returns the little-endian
+    byte sequence of length"""
+    return n.to_bytes(length, 'little')
+
+def read_varint(s):
+    i = s.read(1)[0]
+    if i == 0xfd:
+        return little_endian_to_int(s.read(2))
+    elif i == 0xfe:
+        return little_endian_to_int(s.read(4))
+    elif i == 0xff:
+        return little_endian_to_int(s.read(8))
+    else:
+        return i
+
+def encode_varint(i):
+    if i < 0xfd:
+        return bytes([i])
+    elif i < 0x10000:
+        return b'\xfd' + int_to_little_endian(i, 2)
+    elif i < 0x10000000:
+        return b'\xfe' + int_to_little_endian(i, 4)
+    elif i < 0x1000000000000000:
+        return b'\xff' + int_to_little_endian(i, 8)
+    else:
+        raise ValueError('integer too large: {}'.format(i))
+
 
